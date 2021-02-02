69e40ee538cc579901cb9c0e660004fd54fd608d
Roy de Jong <roy@softwarepunt.nl>
2021-02-02 16:11:52 +0100

    docs: set theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,60 +1,28 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
+import sphinx_rtd_theme
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import recommonmark
+from recommonmark.transform import AutoStructify
 
-
-# -- Project information -----------------------------------------------------
+# ----- Project information --------------------------------------------------------------------------------------------
 
 project = 'Stim.js'
 copyright = '2021, Roy de Jong'
 author = 'Roy de Jong'
 
+# ----- General configuration ------------------------------------------------------------------------------------------
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['recommonmark']
-
-# Add any paths that contain templates here, relative to this directory.
+extensions = ['recommonmark', 'sphinx_rtd_theme']
 templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# ----- Options for HTML output ----------------------------------------------------------------------------------------
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-########################################################################################################################
-# AutoStructify for table of contents via markdown
-
-import recommonmark
-from recommonmark.transform import AutoStructify
+# ----- AutoStructify for table of contents ----------------------------------------------------------------------------
 
 def setup(app):
     app.add_config_value('recommonmark_config', {
